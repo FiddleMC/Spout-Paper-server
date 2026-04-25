99988170375b994cbd5d9e24bc8e10fd2bf161a8
Martijn Muijsers <martijnmuijsers@live.nl>
2026-04-25 14:00:57 +0200

    [ci-skip] Update latest version in README

--- test-plugin/build.gradle.kts
+++ test-plugin/build.gradle.kts
@@ -3,7 +3,7 @@ version = "1.0.0-SNAPSHOT"
 // Uncomment the lines below to include the dev bundle
 // (after having published it by calling the script at gradle-bin/refreshTestPluginDevBundle)
 // plugins { id("io.papermc.paperweight.userdev") version "2.0.0-beta.19" }
-// dependencies { paperweight.paperDevBundle("1.21.11-R0.2-SNAPSHOT", "spout.server.paper") }
+// dependencies { paperweight.paperDevBundle(rootProject.version, "spout.server.paper") }
 
 repositories {
     mavenLocal()
